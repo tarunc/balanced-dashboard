0f02f5ac1a48fed761a8b887260c0af677e1de19
Tarun Chaudhry <tarunc92@gmail.com>
2014-03-06 16:35:34 -0800

    select text

--- app/views/_base.js
+++ app/views/_base.js
@@ -4,10 +4,34 @@ Balanced.View = Ember.View.extend({
 		this._super();
 
 		this.$('form input[type=text]:first').focus();
+		this.$('.selectable').on('click', $.proxy(this.selectText, this));
 	},
 
 	willDestroyElement: function() {
 		this.set('elementInDom', false);
 		this._super();
+	},
+
+	selectText: function(evt) {
+		if (document.selection) {
+			var range;
+
+			if (document.body.createTextRange) {
+				range = document.body.createTextRange();
+			} else if (document.selection.createRange) {
+				range = document.selection.createRange();
+			}
+
+			if (!range) {
+				return;
+			}
+
+			range.moveToElementText(evt.target);
+			range.select();
+		} else if (window.getSelection) {
+			var range = document.createRange();
+			range.selectNode(evt.target);
+			window.getSelection().addRange(range);
+		}
 	}
 });
